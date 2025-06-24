b5b2a25a21428b2eaadc8328ecd3a5f85515d3ac
َAmirhosein <amirhoosenbabai82@gmail.com>
2025-06-24 14:12:18 +0330

    Refactor: Split Django settings into base, dev, and prod modules
    - Removed monolithic settings.py and replaced with modular structure:
    
      - base.py: shared settings
    
      - dev.py: development environment
    
      - prod.py: production environment
    
    - Updated manage.py and wsgi.py to support modular settings
    
    - Added dotenv loading to support environment-based config
    
    - Adjusted requirements.txt to ensure compatibility with dotenv and environments

---
+++ backend/backend/settings/prod.py
@@ -0,0 +1,15 @@
+from .base import *
+
+# Database configuration(postgres)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': os.getenv("POSTGRES_HOST", "db"),
+        'PORT': os.getenv("POSTGRES_PORT", "5432"),
+    }
+}
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,16 +1,18 @@
-"""
-WSGI config for backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+from dotenv import load_dotenv
+
+# Load environment variables from a .env file
+load_dotenv()
+
+# Read the DJANGO_ENV environment variable to determine the current environment (e.g., 'dev', 'prod').
+# If DJANGO_ENV is not set, default to 'dev'.
+env = os.getenv('DJANGO_ENV', 'dev')  
+
+# Set the Django settings module based on the environment.
+# For example, this could result in 'backend.settings.dev' or 'backend.settings.prod'.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'backend.settings.{env}')
 
 application = get_wsgi_application()
